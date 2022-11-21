a91686fa41fdf9bae0c0b1c01ccac95c74558901
damien.morard <damien.morard@unige.ch>
2022-11-21 15:08:05 +0100

    Forget to manage the case empty set for pprint function on sets of intervals

--- Sources/SIDD/FSI/SetIntervals+Extension.swift
+++ Sources/SIDD/FSI/SetIntervals+Extension.swift
@@ -1,6 +1,9 @@
 extension SetIntervals: CustomStringConvertible {
   public var description: String {
     if let set = self.setIntervals {
+      if set == [] {
+        return "[]"
+      }
       var res: String = "{"
       for s in set {
         res.append("\(s), ")
