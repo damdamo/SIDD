93908813c1195d39cc2f6b336cbf4a133b1eb404
damien.morard <damien.morard@unige.ch>
2022-11-22 16:23:39 +0100

    Remove useless code

--- Sources/SIDD/FSI/Interval+Extension.swift
+++ Sources/SIDD/FSI/Interval+Extension.swift
@@ -25,36 +25,3 @@ extension Interval: CustomStringConvertible {
     return res
   }
 }
-
-//extension Lbracket: CustomStringConvertible {
-//  public var description: String {
-//    switch self {
-//    case .e:
-//      return "("
-//    case .i:
-//      return "["
-//    }
-//  }
-//}
-//
-//extension Rbracket: CustomStringConvertible {
-//  public var description: String {
-//    switch self {
-//    case .e:
-//      return ")"
-//    case .i:
-//      return "]"
-//    }
-//  }
-//}
-//
-//extension Interval: CustomStringConvertible {
-//  public var description: String {
-//    switch self {
-//    case .empty:
-//      return "Ø"
-//    case .intvl(lbracket: let l, a: let a, b: let b, rbracket: let r):
-//      return "\(l)\(a),\(b)\(r)"
-//    }
-//  }
-//}
